b8d36f29b6ca7df34760dd6bf1b4e5b45b9d47a9
Lukáš Vaniščák <lukas.vaniscak@student.tuke.sk>
2018-01-03 11:38:45 +0100

    Registration Form validation add validating rules for hobbies

--- app/Http/Requests/RegistrationFormRequest.php
+++ app/Http/Requests/RegistrationFormRequest.php
@@ -10,29 +10,29 @@ class RegistrationFormRequest extends FormRequest
 {
     public function rules()
     {
-
         return [
             'email' => 'bail|required|unique:users',
             'name' => 'required',
-            'swimming' => 'required',
-            'cycling' => 'required',
-            'running' => 'required',
-            'tourism' => 'required',
-            'climbing' => 'required',
+            'swimming' => 'required|integer|between:1,5',
+            'cycling' => 'required|integer|between:1,5',
+            'running' => 'required|integer|between:1,5',
+            'tourism' => 'required|integer|between:1,5',
+            'climbing' => 'required|integer|between:1,5',
             ];
     }
 
     public function messages()
     {
         return [
-            'email.required' => 'Zadajte emailovú adresu !',
-            'email.unique' => 'Tento email je už používaný !',
-            'name.required' => 'Zadajte meno !',
-            'swimming.required' => 'Vyberte ako sa vám páči plávanie !',
-            'cycling.required' => 'Vyberte ako sa vám páči byciklovanie !',
-            'running.required' => 'Vyberte ako sa vám páči beh !',
-            'tourism.required' => 'Vyberte ako sa vám páči turistika !',
-            'climbing.required' => 'Vyberte ako sa vám páči lezenie !',
+            'email.required' => 'Zadajte emailovú adresu!',
+            'email.unique' => 'Tento email je už používaný!',
+            'name.required' => 'Zadajte meno!',
+            'swimming.required' => 'Vyberte ako sa vám páči plávanie!',
+            'cycling.required' => 'Vyberte ako sa vám páči byciklovanie!',
+            'running.required' => 'Vyberte ako sa vám páči beh!',
+            'tourism.required' => 'Vyberte ako sa vám páči turistika!',
+            'climbing.required' => 'Vyberte ako sa vám páči lezenie!',
+            'between' => 'Vyberte ako veľmi máte radi túto záľubu!',
         ];
     }
 
