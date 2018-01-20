6de6235c66284e1bcd516caa9454aecb61659bda
Lukáš Vaniščák <lukas.vaniscak@student.tuke.sk>
2018-01-20 20:19:36 +0100

    HobbyController refactored exception handling and using repository

--- app/Http/Controllers/HobbyController.php
+++ app/Http/Controllers/HobbyController.php
@@ -5,22 +5,34 @@ namespace App\Http\Controllers;
 use App\Hobbies\HobbyComparator;
 use App\Http\Requests\CompareFormRequest;
 use App\Repositories\EloquentUserRepository;
+use Illuminate\Support\Facades\Log;
+use Psr\Log\InvalidArgumentException;
 use Validator;
 
 class HobbyController extends Controller
 {
+    protected $userRepository;
+
+    public function __construct(EloquentUserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     public function compare(CompareFormRequest $request)
     {
 
 
         try {
-            $list = (new HobbyComparator(new EloquentUserRepository()))->compare($request->email);
-            if ($list instanceof \Exception) {
-                throw $list;
+            if(!$request->email){
+                throw new InvalidArgumentException("Email missing");
             }
+            $list = (new HobbyComparator($this->userRepository))->compare($request->email);
 
         } catch (\Exception $e) {
-            return $e->getMessage();
+            Log::error($e->getMessage());
+            return redirect('hobbies/compare')
+                ->withErrors(['email' => 'Nastal problem počas spracovania.'])
+                ->withInput();
         }
         if ($list == null) {
             return redirect('hobbies/compare')
