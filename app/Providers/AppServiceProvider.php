ed2253579249e6b9f3b81ecdf65f4e2ce1ec0156
Lukáš Vaniščák <lukas.vaniscak@student.tuke.sk>
2018-01-20 19:38:36 +0100

    AppServiceProvider remove unused  bind

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind('App\Repositories\Contracts\UserRepositoryInterface', 'App\Repositories\EloquentUserRepository');
+
     }
 }
